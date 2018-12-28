187f5a7df69d73cc373347265e79c67c748464ff
William Pembleton <william.pembleton17@ncf.edu>
2018-12-28 09:41:52 -0500

    Found a light accent color and worked on the new task activity

--- App/app/src/main/java/com/williampembleton/milestone/TaskList.java
+++ App/app/src/main/java/com/williampembleton/milestone/TaskList.java
@@ -1,5 +1,6 @@
 package com.williampembleton.milestone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,8 +23,13 @@ public class TaskList extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(TaskList.this, NewTask.class);
+                startActivity(intent);
+
+                /*
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                */
             }
         });
     }
